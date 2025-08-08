f58d5249ad2c9b1b28e200322f837946583a94c8
chrstion <christ_andre@hotmail.fr>
2025-08-08 11:01:41 +0000

    Just add  a space on config file

--- src/config.js
+++ src/config.js
@@ -4,3 +4,4 @@ const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
 const SMALL_IMG_BASE_URL = "https://image.tmdb.org/t/p/w300/";
 
 export { BASE_URL, BACKDROP_BASE_URL, SMALL_IMG_BASE_URL };
+
